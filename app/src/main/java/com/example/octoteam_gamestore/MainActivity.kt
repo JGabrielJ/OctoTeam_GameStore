fe2556288b0c25f72d0f5c205542073fcbb83261
Cauan87 <pedro.cauan.sousa62@aluno.ifce.edu.br>
2023-05-16 17:07:38 -0300

    Testando...

--- app/src/main/java/com/example/octoteam_gamestore/MainActivity.kt
+++ app/src/main/java/com/example/octoteam_gamestore/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.octoteam_gamestore
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-// MainActivity1
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
