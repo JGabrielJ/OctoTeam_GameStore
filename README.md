5f219f47eedaf88d24e2767b88f2134efcb725f2
Cauan87 <pedro.cauan.sousa62@aluno.ifce.edu.br>
2023-05-16 16:59:14 -0300

    Front-End parcialmente terminado.

---
+++ README.md
@@ -0,0 +1,2 @@
+# OctoTeam_GameStore
+Um aplicativo que simula uma loja de jogos criada pela equipe Octo Team na disciplina de Programação de Dispositivos Móveis II, ministrada pelo professor Alisson Gomes Linhares.
---
+++ app/src/main/java/com/example/octoteam_gamestore/MainPerson.kt
@@ -0,0 +1,12 @@
+package com.example.octoteam_gamestore
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainPerson : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+         setContentView(R.layout.activity_person)
+    }
+}
