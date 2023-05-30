34b12337785f850fb7b1d182eaa26df03d6fc704
JGabrielJ <gjacinto0707@gmail.com>
2023-05-30 17:23:51 -0300

    Finalizando a FragmentDesc e complementando a FragmentProf

--- app/src/main/java/com/example/octoteam_gamestore/MainActivity.kt
+++ app/src/main/java/com/example/octoteam_gamestore/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.octoteam_gamestore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.example.octoteam_gamestore.databinding.ActivityMainBinding
 
-// MainActivity
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(FragmentDesc())
+
+        binding.navBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home_nav_bar -> replaceFragment(FragmentDesc())
+                R.id.fav_nav_bar -> replaceFragment(FragmentFav())
+                R.id.prof_nav_bar -> replaceFragment(FragmentProf())
+                else -> {}
+            }
+
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
