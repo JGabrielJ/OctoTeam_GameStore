0b35aba75b924ae2ac529392450d18171f8489e7
Murilo Rodrigues Maia <murilo.maia62@aluno.ifce.edu.br>
2023-05-30 23:24:25 -0300

    terminei de fazer o tab layout, mas ainda falta fazer o fragmento dos favoritos

--- app/src/main/java/com/example/octoteam_gamestore/MainActivity.kt
+++ app/src/main/java/com/example/octoteam_gamestore/MainActivity.kt
@@ -2,12 +2,36 @@ package com.example.octoteam_gamestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 // MainActivity1
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var adapter: libraryFavoriteAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabLayout = findViewById(R.id.tab)
+        viewPager2 = findViewById(R.id.viewpager)
+
+        adapter = libraryFavoriteAdapter(supportFragmentManager,lifecycle)
+        viewPager2.adapter = adapter
+
+        TabLayoutMediator(tabLayout,viewPager2){tab,position->
+            when (position){
+                0 ->{
+                    tab.text = "Biblioteca"
+                }
+                1 ->{
+                    tab.text = "Favoritos"
+                }
+            }
+        }.attach()
     }
 }
